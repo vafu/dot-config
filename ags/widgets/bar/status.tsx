f6d9d2224904f2d560eae2fcedfd99ce48b33264
Valentyn <valentyn.fuchedzhy@gmail.com>
2025-03-26 22:09:07 -0700

    add kb batt

--- ags/widgets/bar/status.tsx
+++ ags/widgets/bar/status.tsx
@@ -1,19 +1,35 @@
-import { exec, Variable } from "astal"
+import { bind, exec, Variable } from 'astal'
+import AstalBluetooth from 'gi://AstalBluetooth'
 
-const CPU = Variable("0")
-  .poll(3000, () => exec("bash scripts/cpu.sh"))
+const CPU = Variable('0').poll(3000, () => exec('bash scripts/cpu.sh'))
 
-const RAM = Variable("0")
-  .poll(3000, () => exec("bash scripts/ram.sh"))
+const RAM = Variable('0').poll(3000, () => exec('bash scripts/ram.sh'))
 
-export const Status = () => <box>
-  <box cssClasses={['bar-widget']}>
-    <image iconName="utilities-system-monitor-symbolic"/>
-    <label label={CPU().as(c => c + "%")} />
+const keeb = AstalBluetooth.get_default()
+  .get_devices()
+  .find((d) => d.name == 'Sofle')
+
+console.log(keeb.batteryPercentage)
+
+export const Status = () => (
+  <box>
+    <box cssClasses={['bar-widget']}>
+      <image iconName="utilities-system-monitor-symbolic" />
+      <label label={CPU().as((c) => c + '%')} />
+    </box>
+
+    <box cssClasses={['bar-widget']}>
+      <image iconName="system-software-install-symbolic" />
+      <label label={RAM().as((c) => c + '%')} />
+    </box>
+
+    <box cssClasses={['bar-widget']}>
+      <image iconName="preferences-desktop-keyboard" />
+      <label
+        label={bind(keeb, 'batteryPercentage').as(
+          (p) => (p * 100).toString() + '%'
+        )}
+      />
+    </box>
   </box>
-
-  <box cssClasses={['bar-widget']}>
-    <image iconName="system-software-install-symbolic"/>
-    <label label={RAM().as(c => c + "%")} />
-  </box>
-</box>
+)
