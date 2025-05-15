8d5b11941643caafeb05fcaea53cd461f9fd65b1
Valentyn <valentyn.fuchedzhy@gmail.com>
2025-05-15 15:36:12 -0700

    monitors fn

--- ags/app.tsx
+++ ags/app.tsx
@@ -1,14 +1,14 @@
 logDebug("importing gtk")
-import { App, Gtk } from 'astal/gtk4'
+import { App, Gdk, Gtk, Widget } from 'astal/gtk4'
 import Adw from 'gi://Adw?version=1'
 import Bar from 'widgets/bar'
 import style from './style/style'
 import OSD from 'widgets/osd'
 import { logDebug } from 'logger'
 import { fromConnectable } from 'rxbinding'
-import { withPrevious } from 'commons/rx'
+import { diffs, withPrevious } from 'commons/rx'
 import AstalHyprland from 'gi://AstalHyprland?version=0.1'
-import { map, retry } from 'rxjs'
+import { map, Observable, retry } from 'rxjs'
 
 App.start({
   css: style,
@@ -20,26 +20,27 @@ App.start({
     s.set_property("gtk-icon-theme-name", "Material")
     console.log("using gtk theme", t.theme_name)
 
-
-    fromConnectable(AstalHyprland.get_default(), 'monitors').pipe(
-      map(monitors => monitors.map(m =>
-        App.get_monitors().find(am =>
-          am.description.startsWith(m.description) || m.description.startsWith(am.description)
-        )
-      )),
-      retry({
-        count: 2,
-        delay: 1000
-      }),
-      withPrevious([]),
-      map(([p, c]) => c.filter(current => !p.includes(current)))
-    ).subscribe(monitors => monitors.forEach(m => Bar(m)))
+    monitors().pipe(diffs()).subscribe(monitors =>
+      monitors.added.forEach(m => Bar(m))
+    )
 
     App.get_monitors().forEach((m) => {
       console.log("Creating OSD for")
       return OSD(m)
     })
-
-    // NotificationPopups(App.get_monitors()[0])
   },
 })
+
+function monitors(): Observable<Gdk.Monitor[]> {
+  return fromConnectable(AstalHyprland.get_default(), 'monitors').pipe(
+    map(monitors => monitors.map(m =>
+      App.get_monitors().find(am =>
+        am.description.startsWith(m.description) || m.description.startsWith(am.description)
+      )
+    )),
+    retry({
+      count: 2,
+      delay: 1000
+    }),
+  )
+}
--- ags/commons/rx.ts
+++ ags/commons/rx.ts
@@ -1,5 +1,6 @@
 import {
   EMPTY,
+  map,
   onErrorResumeNextWith,
   OperatorFunction,
   publish,
@@ -24,6 +25,40 @@ export function withPrevious<T>(initial: T): OperatorFunction<T, T[]> {
       publish(shared =>
         shared.pipe(zipWith(shared.pipe(skip(1))))
       )
+    )
+}
+
+type Diff<T> = {
+  added: T[]
+  removed: T[]
+  unchanged: T[]
+}
+
+export function diffs<T>(): OperatorFunction<T[], Diff<T>> {
+  return (source) =>
+    source.pipe(
+      withPrevious([]),
+      map(([p, c]) => {
+        const added: T[] = []
+        const removed: T[] = []
+        const unchanged: T[] = []
+
+        for (const prev of p) {
+          if (c.includes(prev)) {
+            unchanged.push(prev)
+          } else {
+            removed.push(prev)
+          }
+        }
+
+        for (const curr of c) {
+          if (!p.includes(curr)) {
+            added.push(curr)
+          }
+        }
+
+        return { added, removed, unchanged }
+      })
     )
 }
 
