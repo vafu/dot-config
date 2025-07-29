ad6eb185e0447d2bc2205a4bc1ae58a18cbe164e
Valentyn <valentyn.fuchedzhy@gmail.com>
2025-07-29 12:49:38 -0700

    workspace swithcher per monitor

--- ags/widgets/bar/workspaces.tsx
+++ ags/widgets/bar/workspaces.tsx
@@ -2,7 +2,7 @@ import { range } from 'commons'
 import Service from 'services'
 import Gtk from 'gi://Gtk?version=4.0'
 import { binding } from 'rxbinding'
-import { combineLatest, filter, map } from 'rxjs'
+import { combineLatest, filter, map, of, switchMap } from 'rxjs'
 import { Gdk } from 'astal/gtk4'
 
 const workspaceService = Service('workspace')
@@ -11,7 +11,16 @@ const workspaces = (props: { monitor: Gdk.Monitor }) =>
   range(7).map(idx => {
     const ws = workspaceService.getWorkspace(idx)
     const active = workspaceService.activeWorkspaceFor(props.monitor).pipe(
-      map(a => a == ws ? 'active' : '')
+      switchMap(w => combineLatest(of(w), workspaceService.activeWorkspace)),
+      map(([active, focused]) => {
+        if (ws == focused && ws == active) {
+          return 'focused'
+        } else if (ws == active) {
+          return 'active'
+        } else {
+          return ''
+        }
+      })
     )
     return (
       <label
