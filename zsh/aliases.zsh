0316cb989db5ba627d24d27f6214f3e932ed2ddf
Valentyn <valentyn.fuchedzhy@gmail.com>
2025-08-21 12:43:45 -0700

    add aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,17 @@
 alias e="$EDITOR"
 alias se="sudoedit"
+#
+alias gpb='git push origin "$(git branch --show-current)"'
+
+alias gaa="git add ."
+alias gcm="git commit -m"
+
+gpa() {
+  if [ -z "$1" ]; then
+    echo "Error: Please provide a commit message."
+    return 1
+  fi
+  gaa
+  gcm $1
+  gpb
+}
