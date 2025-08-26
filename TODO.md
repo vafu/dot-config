e0a2fbbe33ee2c75c17c1c177ee78d4c46a7370a
Valentyn <valentyn.fuchedzhy@gmail.com>
2025-08-25 22:26:00 -0700

    add todo

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# Hyprland
+-[ ] Single branch for both color schemes
+-[ ] Disable blur/transparency on battery saving
+
+# AGS
+-[ ] Notes, maybe integrate with obsidian?
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -15,3 +15,9 @@ gpa() {
   gcm $1
   gpb
 }
+
+function reload_gtk_theme() {
+  theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
+  gsettings set org.gnome.desktop.interface gtk-theme ''
+  gsettings set org.gnome.desktop.interface gtk-theme $theme
+}
