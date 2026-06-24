220295c760ddd559068b9f2945e5441914b9c1b0
Valentyn <valentyn.fuchedzhy@gmail.com>
2026-06-24 14:20:54 -0700

    scripts and stuff

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,3 +1,4 @@
 export GOPATH=$HOME/.go
 export PATH=$HOME/.local/bin/:$HOME/.cargo/bin/:$GOPATH/bin:$HOME/.config/scripts/:$HOME/ai/bin/:$PATH
 export EDITOR=nvim
+export LOCUS_ROOT=$XDG_RUNTIME_DIR/locusfs
